66a0eaf0c879426271fb0c34ffaf413971203cc4
Andrej <a.sushko@gmail.com>
2023-04-19 17:41:29 +0200

    fix2

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template, request, redirect
-import requests, random, os, time
-from apscheduler.schedulers.background import BackgroundScheduler
-import datetime
 import atexit
+import os
+import random
+import redis
+import requests
 import telepot
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-word_list = [(160, 'AAPL', 162, None, False, False), (104, 'AMZN', 105, None, False, False)]
-# apikeys_list = ['0c471a777f91b810299e5e3981522450','c487c7f54a0fdefa58f92596372acaf7',
-#                 'ba46d09d77ac077b7bcbeab874df2ab5', '313e40cba8245e1accc76a64d3c241ac']
+REDIS_HASH_KEY = 'stock_data'
 
-apikeys_list = ['34d3f176e718e216ee8560a150cb0000']
-telegram_token  = os.getenv('TELEG_TOCKEN')
-refreshDelay  = int(os.getenv('API_REFRESH', 60))
+word_list = [(277, 'MSFT', 193, None, False, False), (100, 'AMZN', 104.2, None, False, False)]
+apikeys_list = ['34d3f176e718e216ee8560a150cb0000', '0c471a777f91b810299e5e3981522450','c487c7f54a0fdefa58f92596372acaf7',
+                'ba46d09d77ac077b7bcbeab874df2ab5', '313e40cba8245e1accc76a64d3c241ac']
+
+# apikeys_list = []
+REDIS_CONNECTION_URL = os.getenv('REDIS_URL')
+telegram_token = os.getenv('TELEG_TOCKEN')
+refreshDelay = int(os.getenv('API_REFRESH', 60))
+
 receiver_id = 729835175
+
+# Connection string
+
+# Connect to the Redis server using the connection string
+redis_connection = redis.StrictRedis.from_url(REDIS_CONNECTION_URL, decode_responses=True)
+# Store initial data in Redis hash
+for low, word, high, price, alertLow, alertHigh in word_list:
+    redis_connection.hset(REDIS_HASH_KEY, word, str((low, high, price, alertLow, alertHigh)))
+
 
 def sendMessage(text):
     bot = telepot.Bot(telegram_token)
@@ -35,20 +51,24 @@ def get_ticker_data(word):
 
 
 def update_ticker_prices():
-    print(word_list)
-    for i, (low, word, high, price, alertLow, alertHigh) in enumerate(word_list):
+    stock_data = redis_connection.hgetall(REDIS_HASH_KEY)
+
+    for word, data_str in stock_data.items():
+        low, high, price, alertLow, alertHigh = eval(data_str)
         new_price = get_ticker_data(word)
+
         if new_price is not None:
             if not alertLow and new_price <= low:
                 sendMessage(f"{word} cross low {low} \n and now is {new_price}")
-                word_list[i] = (low, word, high, new_price, True, alertHigh)
+                alertLow = True
             elif not alertHigh and new_price >= high:
                 sendMessage(f"{word} cross high {high} \n and now is {new_price}")
-                word_list[i] = (low, word, high, new_price, alertLow, True)
-            else:
-                word_list[i] = (low, word, high, new_price, alertLow, alertHigh)
+                alertHigh = True
 
+            # Update the data in the Redis hash
+            redis_connection.hset(REDIS_HASH_KEY, word, str((low, high, new_price, alertLow, alertHigh)))
 
+update_ticker_prices()
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=update_ticker_prices, trigger="interval", seconds= refreshDelay)
 scheduler.start()
@@ -56,22 +76,26 @@ scheduler.start()
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
 
-# @app.context_processor
+@app.context_processor
 def inject_ticker_data():
-    if request.path == '/':
-        return {}
+
     ticker_data = {}
-    for low, word, high, price in word_list:
-        if not price:
-            price = get_ticker_data(word)
-            for i, (low, w, high, _) in enumerate(word_list):
-                if w == word:
-                    word_list[i] = (low, w, high, price)
+    stock_data = redis_connection.hgetall(REDIS_HASH_KEY)
+
+    for word, data_str in stock_data.items():
+        low, high, price, alertLow, alertHigh = eval(data_str)
         ticker_data[word] = price
     return {'ticker_data': ticker_data}
 
 @app.route('/')
 def index():
+    stock_data = redis_connection.hgetall(REDIS_HASH_KEY)
+    word_list = []
+
+    for word, data_str in stock_data.items():
+        low, high, price, alertLow, alertHigh = eval(data_str)
+        word_list.append((low, word, high, price, alertLow, alertHigh))
+
     return render_template('index.html', word_list=word_list)
 
 @app.route('/add_word', methods=['POST'])
@@ -79,16 +103,13 @@ def add_word():
     word = request.form['word']
     low = float(request.form['low'])
     high = float(request.form['high'])
-    word_list.append((low, word, high, None, False, False))
+    redis_connection.hset(REDIS_HASH_KEY, word, str((low, high, None, False, False)))
     return redirect('/')
 
 @app.route('/remove_word', methods=['POST'])
 def remove_word():
     word = request.form['word']
-    for i, (low, w, high, price, alertLow, alertHigh) in enumerate(word_list):
-        if w == word:
-            word_list.pop(i)
-            break
+    redis_connection.hdel(REDIS_HASH_KEY, word)
     return redirect('/')
 
 if __name__ == '__main__':
